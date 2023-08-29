e8bb091db258363a5027cab0ae65e2f7dff72670
Joey Palchak <joey.palchak@gmail.com>
2023-08-29 14:29:16 -0700

    Refactor tests for Problem 1.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,20 @@
 import { solveMultiples } from './../src/index.js';
 
-describe('solveMutliples', () => {
+describe('solveMultiples', () => {
 
-  test('should return the result of 9 modulo 3', () => {
-    const answer = solveMultiples(9, 3);
-    expect(answer).toEqual(0);
-  });
+  // test('should return the result of 9 modulo 3', () => {
+  //   const answer = solveMultiples(9, 3);
+  //   expect(answer).toEqual(0);
+  // });
 
   test('should return an array of the numbers 1 to 9', () => {
-    const answer = solveMultiples(9,3);
+    const answer = solveMultiples(9);
     expect(answer).toEqual([1,2,3,4,5,6,7,8,9]);
+  });
+
+  test('should return an array of numbers divisible by three', () => {
+    const answer = solveMultiples(9);
+    expect(answer).toEqual([3,6,9]);
   });
 
 });
