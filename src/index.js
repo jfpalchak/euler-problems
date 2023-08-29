d09d61a880e958f4c0d2d2de605ceed4fd3da4ac
Joey Palchak <joey.palchak@gmail.com>
2023-08-29 14:12:33 -0700

    Pass Test 2 for Problem 1.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import './css/styles.css';
+//import './css/styles.css';
 
 
 export function solveMultiples(num1, num2) {
-  return num1 % num2;
+  // return num1 % num2;
+  const num1Array = [];
+  for (let i = 1; i < num1; i++) {
+    num1Array.push(i);
+  }
+  
+  return num1Array;
 }
 
